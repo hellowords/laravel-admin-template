886384a4e085304ac39590fa7a25f20733f565c8
happyhack <drinke9@happyhack.cn>
2018-11-28 21:14:38 +0800

    修改注册后页面重定向

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin';
 
     /**
      * Create a new controller instance.
